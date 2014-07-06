9b36cbc42874532bf84b4f0576b498191641636e
Philippe Marion <contact@villagedefrance.net>
2014-07-06 14:33:58 +0100

    Update index.php

--- upload/admin/index.php
+++ upload/admin/index.php
@@ -4,68 +4,68 @@
 //------------------------
 
 // Version
-define('VERSION', '1.5.6.4 - Overclocked'); 
+define('VERSION', '1.5.6.4 - Overclocked');
 define('EDITION', 'Basic Edition');
-define('BUILD', '1.1.0'); 
+define('BUILD', '1.1.0');
 
 // Configuration
-if (file_exists('config.php')) { 
-	require_once('config.php'); 
-} 
+if (file_exists('config.php')) {
+	require_once('config.php');
+}
 
 // Install
-if (!defined('DIR_APPLICATION')) { 
-	header('Location: ../install/index.php'); 
-	exit; 
-} 
+if (!defined('DIR_APPLICATION')) {
+	header('Location: ../install/index.php');
+	exit;
+}
 
 // Startup
-require_once(DIR_SYSTEM . 'startup.php'); 
+require_once(DIR_SYSTEM . 'startup.php');
 
 // Application Classes
-require_once(DIR_SYSTEM . 'library/currency.php'); 
-require_once(DIR_SYSTEM . 'library/user.php'); 
-require_once(DIR_SYSTEM . 'library/weight.php'); 
-require_once(DIR_SYSTEM . 'library/length.php'); 
+require_once(DIR_SYSTEM . 'library/currency.php');
+require_once(DIR_SYSTEM . 'library/user.php');
+require_once(DIR_SYSTEM . 'library/weight.php');
+require_once(DIR_SYSTEM . 'library/length.php');
 
 // Registry
-$registry = new Registry(); 
+$registry = new Registry();
 
 // Loader
-$loader = new Loader($registry); 
-$registry->set('load', $loader); 
+$loader = new Loader($registry);
+$registry->set('load', $loader);
 
 // Config
-$config = new Config(); 
-$registry->set('config', $config); 
+$config = new Config();
+$registry->set('config', $config);
 
 // Database
-$db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE); 
-$registry->set('db', $db); 
+$db = new DB(DB_DRIVER, DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+$registry->set('db', $db);
 
 // Settings
-$query = $db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE store_id = '0'"); 
+$query = $db->query("SELECT * FROM " . DB_PREFIX . "setting WHERE store_id = '0'");
 
-foreach ($query->rows as $setting) { 
-	if (!$setting['serialized']) { 
-		$config->set($setting['key'], $setting['value']); 
-	} else { 
-		$config->set($setting['key'], unserialize($setting['value'])); 
-	} 
-} 
+foreach ($query->rows as $setting) {
+	if (!$setting['serialized']) {
+		$config->set($setting['key'], $setting['value']);
+	} else {
+		$config->set($setting['key'], unserialize($setting['value']));
+	}
+}
 
 // Url
-$url = new Url(HTTP_SERVER, $config->get('config_secure') ? HTTPS_SERVER : HTTP_SERVER); 
-$registry->set('url', $url); 
+$url = new Url(HTTP_SERVER, $config->get('config_secure') ? HTTPS_SERVER : HTTP_SERVER);
+$registry->set('url', $url);
 
 // Log
-$log = new Log($config->get('config_error_filename')); 
-$registry->set('log', $log); 
+$log = new Log($config->get('config_error_filename'));
+$registry->set('log', $log);
 
-function error_handler($errno, $errstr, $errfile, $errline) { 
-	global $log, $config; 
+function error_handler($errno, $errstr, $errfile, $errline) {
+	global $log, $config;
 
-	switch ($errno) { 
+	switch ($errno) {
 		case E_NOTICE:
 		case E_USER_NOTICE:
 			$error = 'Notice';
@@ -81,93 +81,93 @@ function error_handler($errno, $errstr, $errfile, $errline) {
 		default:
 			$error = 'Unknown';
 		break;
-	} 
+	}
 
-	if ($config->get('config_error_display')) { 
-		echo '<b>' . $error . '</b>: ' . $errstr . ' in <b>' . $errfile . '</b> on line <b>' . $errline . '</b>'; 
-	} 
+	if ($config->get('config_error_display')) {
+		echo '<b>' . $error . '</b>: ' . $errstr . ' in <b>' . $errfile . '</b> on line <b>' . $errline . '</b>';
+	}
 
-	if ($config->get('config_error_log')) { 
-		$log->write('PHP ' . $error . ': ' . $errstr . ' in ' . $errfile . ' on line ' . $errline); 
-	} 
+	if ($config->get('config_error_log')) {
+		$log->write('PHP ' . $error . ': ' . $errstr . ' in ' . $errfile . ' on line ' . $errline);
+	}
 
-	return true; 
-} 
+	return true;
+}
 
 // Error Handler
-set_error_handler('error_handler'); 
+set_error_handler('error_handler');
 
 // Request
-$request = new Request(); 
-$registry->set('request', $request); 
+$request = new Request();
+$registry->set('request', $request);
 
 // Response
-$response = new Response(); 
-$response->addHeader('Content-Type: text/html; charset=utf-8'); 
-$registry->set('response', $response); 
+$response = new Response();
+$response->addHeader('Content-Type: text/html; charset=utf-8');
+$registry->set('response', $response);
 
 // Cache
-$cache = new Cache(); 
-$registry->set('cache', $cache); 
+$cache = new Cache();
+$registry->set('cache', $cache);
 
 // Session
-$session = new Session(); 
-$registry->set('session', $session); 
+$session = new Session();
+$registry->set('session', $session);
 
 // Language
-$languages = array(); 
+$languages = array();
 
-$query = $db->query("SELECT * FROM `" . DB_PREFIX . "language`"); 
+$query = $db->query("SELECT * FROM `" . DB_PREFIX . "language`");
 
-foreach ($query->rows as $result) { 
-	$languages[$result['code']] = $result; 
-} 
+foreach ($query->rows as $result) {
+	$languages[$result['code']] = $result;
+}
 
-$config->set('config_language_id', $languages[$config->get('config_admin_language')]['language_id']); 
+$config->set('config_language_id', $languages[$config->get('config_admin_language')]['language_id']);
 
 // Language
-$language = new Language($languages[$config->get('config_admin_language')]['directory']); 
-$language->load($languages[$config->get('config_admin_language')]['filename']); 
+$language = new Language($languages[$config->get('config_admin_language')]['directory']);
+$language->load($languages[$config->get('config_admin_language')]['filename']);
 
-$registry->set('language', $language); 
+$registry->set('language', $language);
 
 // Document
-$registry->set('document', new Document()); 
+$registry->set('document', new Document());
 
 // Currency
-$registry->set('currency', new Currency($registry)); 
+$registry->set('currency', new Currency($registry));
 
 // Weight
-$registry->set('weight', new Weight($registry)); 
+$registry->set('weight', new Weight($registry));
 
 // Length
-$registry->set('length', new Length($registry)); 
+$registry->set('length', new Length($registry));
 
 // User
-$registry->set('user', new User($registry)); 
+$registry->set('user', new User($registry));
 
 //OpenBay Pro
-$registry->set('openbay', new Openbay($registry)); 
+$registry->set('openbay', new Openbay($registry));
 
 // Front Controller
-$controller = new Front($registry); 
+$controller = new Front($registry);
 
 // Login
-$controller->addPreAction(new Action('common/home/login')); 
+$controller->addPreAction(new Action('common/home/login'));
 
 // Permission
-$controller->addPreAction(new Action('common/home/permission')); 
+$controller->addPreAction(new Action('common/home/permission'));
 
 // Router
-if (isset($request->get['route'])) { 
-	$action = new Action($request->get['route']); 
-} else { 
-	$action = new Action('common/home'); 
-} 
+if (isset($request->get['route'])) {
+	$action = new Action($request->get['route']);
+} else {
+	$action = new Action('common/home');
+}
 
 // Dispatch
-$controller->dispatch($action, new Action('error/not_found')); 
+$controller->dispatch($action, new Action('error/not_found'));
 
 // Output
-$response->output(); 
+$response->output();
 ?>
