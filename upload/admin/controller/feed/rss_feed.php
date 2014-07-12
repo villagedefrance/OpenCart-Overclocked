5abc7b50ecef338435502d28da97b10ecfb93485
Philippe Marion <contact@villagedefrance.net>
2014-07-12 18:23:37 +0100

    Admin Controller files updates
    Admin Controller files updates

--- upload/admin/controller/feed/rss_feed.php
+++ upload/admin/controller/feed/rss_feed.php
@@ -1,151 +1,146 @@
 <?php
-//------------------------
-// Overclocked Edition		
-//------------------------
+class ControllerFeedRSSFeed extends Controller {
+	private $error = array();
+	private $_name = 'rss_feed';
 
-class ControllerFeedRSSFeed extends Controller { 
-	private $error = array(); 
-	private $_name = 'rss_feed'; 
+	public function index() {
+		$this->language->load('feed/' . $this->_name);
 
-	public function index() { 
+		$this->document->setTitle($this->language->get('heading_title'));
 
-		$this->language->load('feed/' . $this->_name); 
+		$this->load->model('setting/setting');
 
-		$this->document->setTitle($this->language->get('heading_title')); 
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && ($this->validate())) {
+			$this->model_setting_setting->editSetting($this->_name, $this->request->post);
 
-		$this->load->model('setting/setting'); 
+			$this->session->data['success'] = $this->language->get('text_success');
 
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && ($this->validate())) { 
-			$this->model_setting_setting->editSetting($this->_name, $this->request->post); 
+			if (isset($this->request->post['apply'])) {
+				$this->redirect($this->url->link('feed/' . $this->_name, 'token=' . $this->session->data['token'], 'SSL'));
+			} else {
+				$this->redirect($this->url->link('extension/feed', 'token=' . $this->session->data['token'], 'SSL'));
+			}
+		}
 
-			$this->session->data['success'] = $this->language->get('text_success'); 
+		if (isset($this->error['warning'])) {
+			$this->data['error_warning'] = $this->error['warning'];
+		} else {
+			$this->data['error_warning'] = '';
+		}
 
-			if (isset($this->request->post['apply'])) { 
-				$this->redirect($this->url->link('feed/' . $this->_name, 'token=' . $this->session->data['token'], 'SSL')); 
-			} else { 
-				$this->redirect($this->url->link('extension/feed', 'token=' . $this->session->data['token'], 'SSL')); 
-			} 
-		} 
+		if (isset($this->error['limit'])) {
+			$this->data['error_limit'] = $this->error['limit'];
+		} else {
+			$this->data['error_limit'] = '';
+		}
 
-		if (isset($this->error['warning'])) { 
-			$this->data['error_warning'] = $this->error['warning']; 
-		} else { 
-			$this->data['error_warning'] = ''; 
-		} 
-
-		if (isset($this->error['limit'])) { 
-			$this->data['error_limit'] = $this->error['limit']; 
-		} else { 
-			$this->data['error_limit'] = ''; 
-		} 
-
-		$this->data['breadcrumbs'] = array(); 
+		$this->data['breadcrumbs'] = array();
 
 		$this->data['breadcrumbs'][] = array(
 			'text'      => $this->language->get('text_home'),
 			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
 			'separator' => false
-		); 
+		);
 
 		$this->data['breadcrumbs'][] = array(
 			'text'      => $this->language->get('text_feed'),
 			'href'      => $this->url->link('feed/' . $this->_name, 'token=' . $this->session->data['token'], 'SSL'),
 			'separator' => ' :: '
-		); 
+		);
 
 		// RSS Feed
-		$this->data['heading_title'] = $this->language->get('heading_title'); 
+		$this->data['heading_title'] = $this->language->get('heading_title');
 
-		$this->data['text_feed'] = $this->language->get('text_feed'); 
-		$this->data['text_enabled'] = $this->language->get('text_enabled'); 
-		$this->data['text_disabled'] = $this->language->get('text_disabled'); 
+		$this->data['text_feed'] = $this->language->get('text_feed');
+		$this->data['text_enabled'] = $this->language->get('text_enabled');
+		$this->data['text_disabled'] = $this->language->get('text_disabled');
 
-		$this->data['entry_status'] = $this->language->get('entry_status'); 
-		$this->data['entry_data_feed'] = $this->language->get('entry_data_feed'); 
-		$this->data['entry_limit'] = $this->language->get('entry_limit'); 
-		$this->data['entry_show_image'] = $this->language->get('entry_show_image'); 
-		$this->data['entry_show_price'] = $this->language->get('entry_show_price'); 
-		$this->data['entry_include_tax'] = $this->language->get('entry_include_tax'); 
+		$this->data['entry_status'] = $this->language->get('entry_status');
+		$this->data['entry_data_feed'] = $this->language->get('entry_data_feed');
+		$this->data['entry_limit'] = $this->language->get('entry_limit');
+		$this->data['entry_show_image'] = $this->language->get('entry_show_image');
+		$this->data['entry_show_price'] = $this->language->get('entry_show_price');
+		$this->data['entry_include_tax'] = $this->language->get('entry_include_tax');
 
-		$this->data['entry_image_size'] = $this->language->get('entry_image_size'); 
+		$this->data['entry_image_size'] = $this->language->get('entry_image_size');
 
-		$this->data['button_save'] = $this->language->get('button_save'); 
-		$this->data['button_apply'] = $this->language->get('button_apply'); 
-		$this->data['button_cancel'] = $this->language->get('button_cancel'); 
+		$this->data['button_save'] = $this->language->get('button_save');
+		$this->data['button_apply'] = $this->language->get('button_apply');
+		$this->data['button_cancel'] = $this->language->get('button_cancel');
 
-		$this->data['action'] = $this->url->link('feed/' . $this->_name, 'token=' . $this->session->data['token'], 'SSL'); 
+		$this->data['action'] = $this->url->link('feed/' . $this->_name, 'token=' . $this->session->data['token'], 'SSL');
 
-		$this->data['cancel'] = $this->url->link('extension/feed', 'token=' . $this->session->data['token'], 'SSL'); 
+		$this->data['cancel'] = $this->url->link('extension/feed', 'token=' . $this->session->data['token'], 'SSL');
 
-		$this->data['data_feed'] = HTTP_CATALOG . 'index.php?route=feed/rss_feed'; 
+		$this->data['data_feed'] = HTTP_CATALOG . 'index.php?route=feed/rss_feed';
 
-		$this->data['token'] = $this->session->data['token']; 
+		$this->data['token'] = $this->session->data['token'];
 
-		if (isset($this->request->post[$this->_name . '_status'])) { 
-			$this->data[$this->_name . '_status'] = $this->request->post[$this->_name . '_status']; 
-		} else { 
-			$this->data[$this->_name . '_status'] = $this->config->get($this->_name . '_status'); 
-		} 
+		if (isset($this->request->post[$this->_name . '_status'])) {
+			$this->data[$this->_name . '_status'] = $this->request->post[$this->_name . '_status'];
+		} else {
+			$this->data[$this->_name . '_status'] = $this->config->get($this->_name . '_status');
+		}
 
-		if (isset($this->request->post[$this->_name . '_limit'])) { 
-			$this->data[$this->_name . '_limit'] = $this->request->post[$this->_name . '_limit']; 
-		} else { 
-			$this->data[$this->_name . '_limit'] = $this->config->get($this->_name . '_limit'); 
-		} 
+		if (isset($this->request->post[$this->_name . '_limit'])) {
+			$this->data[$this->_name . '_limit'] = $this->request->post[$this->_name . '_limit'];
+		} else {
+			$this->data[$this->_name . '_limit'] = $this->config->get($this->_name . '_limit');
+		}
 
-		if (isset($this->request->post[$this->_name . '_show_image'])) { 
-			$this->data[$this->_name . '_show_image'] = $this->request->post[$this->_name . '_show_image']; 
-		} else { 
-			$this->data[$this->_name . '_show_image'] = $this->config->get($this->_name . '_show_image'); 
-		} 
+		if (isset($this->request->post[$this->_name . '_show_image'])) {
+			$this->data[$this->_name . '_show_image'] = $this->request->post[$this->_name . '_show_image'];
+		} else {
+			$this->data[$this->_name . '_show_image'] = $this->config->get($this->_name . '_show_image');
+		}
 
-		if (isset($this->request->post[$this->_name . '_show_price'])) { 
-			$this->data[$this->_name . '_show_price'] = $this->request->post[$this->_name . '_show_price']; 
-		} else { 
-			$this->data[$this->_name . '_show_price'] = $this->config->get($this->_name . '_show_price'); 
-		} 
+		if (isset($this->request->post[$this->_name . '_show_price'])) {
+			$this->data[$this->_name . '_show_price'] = $this->request->post[$this->_name . '_show_price'];
+		} else {
+			$this->data[$this->_name . '_show_price'] = $this->config->get($this->_name . '_show_price');
+		}
 
-		if (isset($this->request->post[$this->_name . '_include_tax'])) { 
-			$this->data[$this->_name . '_include_tax'] = $this->request->post[$this->_name . '_include_tax']; 
-		} else { 
-			$this->data[$this->_name . '_include_tax'] = $this->config->get($this->_name . '_include_tax'); 
-		} 
+		if (isset($this->request->post[$this->_name . '_include_tax'])) {
+			$this->data[$this->_name . '_include_tax'] = $this->request->post[$this->_name . '_include_tax'];
+		} else {
+			$this->data[$this->_name . '_include_tax'] = $this->config->get($this->_name . '_include_tax');
+		}
 
-		if (isset($this->request->post[$this->_name . '_image_width'])) { 
-			$this->data[$this->_name . '_image_width'] = $this->request->post[$this->_name . '_image_width']; 
-		} else { 
-			$this->data[$this->_name . '_image_width'] = $this->config->get($this->_name . '_image_width') ? $this->config->get($this->_name . '_image_width') : 100; 
-		} 
+		if (isset($this->request->post[$this->_name . '_image_width'])) {
+			$this->data[$this->_name . '_image_width'] = $this->request->post[$this->_name . '_image_width'];
+		} else {
+			$this->data[$this->_name . '_image_width'] = $this->config->get($this->_name . '_image_width') ? $this->config->get($this->_name . '_image_width') : 100;
+		}
 
-		if (isset($this->request->post[$this->_name . '_image_height'])) { 
-			$this->data[$this->_name . '_image_height'] = $this->request->post[$this->_name . '_image_height']; 
-		} else { 
-			$this->data[$this->_name . '_image_height'] = $this->config->get($this->_name . '_image_height') ? $this->config->get($this->_name . '_image_height') : 100; 
-		} 
+		if (isset($this->request->post[$this->_name . '_image_height'])) {
+			$this->data[$this->_name . '_image_height'] = $this->request->post[$this->_name . '_image_height'];
+		} else {
+			$this->data[$this->_name . '_image_height'] = $this->config->get($this->_name . '_image_height') ? $this->config->get($this->_name . '_image_height') : 100;
+		}
 
-		$this->template = 'feed/' . $this->_name . '.tpl'; 
+		$this->template = 'feed/' . $this->_name . '.tpl';
 		$this->children = array(
 			'common/header',
 			'common/footer'
-		); 
+		);
 
-		$this->response->setOutput($this->render()); 
-	} 
+		$this->response->setOutput($this->render());
+	}
 
-	private function validate() { 
-		if (!$this->user->hasPermission('modify', 'feed/' . $this->_name)) { 
-			$this->error['warning'] = $this->language->get('error_permission'); 
-		} 
+	private function validate() {
+		if (!$this->user->hasPermission('modify', 'feed/' . $this->_name)) {
+			$this->error['warning'] = $this->language->get('error_permission');
+		}
 
-		if ((!$this->request->post[$this->_name . '_limit']) || (!is_numeric($this->request->post[$this->_name . '_limit']))) { 
-			$this->error['limit'] = $this->language->get('error_integer'); 
-		} 
+		if ((!$this->request->post[$this->_name . '_limit']) || (!is_numeric($this->request->post[$this->_name . '_limit']))) {
+			$this->error['limit'] = $this->language->get('error_integer');
+		}
 
-		if (!$this->error) { 
-			return TRUE; 
-		} else { 
-			return FALSE; 
-		} 
-	} 
-} 
+		if (!$this->error) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
 ?>
