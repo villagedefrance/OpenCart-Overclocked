5f06ce673a93c5a92ef8c0e30feba43c97c033f4
Philippe Marion <contact@villagedefrance.net>
2014-07-17 20:51:34 +0100

    Catalog Success Icon Links
    Catalog Success Icon Links

--- upload/catalog/controller/account/success.php
+++ upload/catalog/controller/account/success.php
@@ -1,64 +1,59 @@
-<?php 
-//------------------------
-// Overclocked Edition		
-//------------------------
+<?php
+class ControllerAccountSuccess extends Controller {
 
-class ControllerAccountSuccess extends Controller { 
+	public function index() {
+		$this->language->load('account/success');
 
-	public function index() { 
+		$this->document->setTitle($this->language->get('heading_title'));
 
-		$this->language->load('account/success'); 
-
-		$this->document->setTitle($this->language->get('heading_title')); 
-
-		$this->data['breadcrumbs'] = array(); 
+		$this->data['breadcrumbs'] = array();
 
 		$this->data['breadcrumbs'][] = array(
-			'text'      => $this->language->get('text_home'),
-			'href'      => $this->url->link('common/home'),
+			'text'  	=> $this->language->get('text_home'),
+			'href' 		=> $this->url->link('common/home'),
 			'separator' => false
-		); 
+		);
 
 		$this->data['breadcrumbs'][] = array(
-			'text'      => $this->language->get('text_account'),
-			'href'      => $this->url->link('account/account', '', 'SSL'),
+			'text'  	=> $this->language->get('text_account'),
+			'href' 		=> $this->url->link('account/account', '', 'SSL'),
 			'separator' => $this->language->get('text_separator')
-		); 
+		);
 
 		$this->data['breadcrumbs'][] = array(
-			'text'      => $this->language->get('text_success'),
-			'href'      => $this->url->link('account/success'),
+			'text' 	=> $this->language->get('text_success'),
+			'href'  	=> $this->url->link('account/success'),
 			'separator' => $this->language->get('text_separator')
 		);
 
 		$this->data['heading_title'] = $this->language->get('heading_title');
 
-		$this->load->model('account/customer_group'); 
+		$this->load->model('account/customer_group');
 
-		$customer_group = $this->model_account_customer_group->getCustomerGroup($this->customer->getCustomerGroupId()); 
+		$customer_group = $this->model_account_customer_group->getCustomerGroup($this->customer->getCustomerGroupId());
 
-		if ($customer_group && !$customer_group['approval']) { 
-			$this->data['text_message'] = sprintf($this->language->get('text_message'), $this->url->link('information/contact')); 
-		} else { 
-			$this->data['text_message'] = sprintf($this->language->get('text_approval'), $this->config->get('config_name'), $this->url->link('information/contact')); 
-		} 
+		if ($customer_group && !$customer_group['approval']) {
+			$this->data['text_message'] = sprintf($this->language->get('text_message'), $this->url->link('information/contact'));
+		} else {
+			$this->data['text_message'] = sprintf($this->language->get('text_approval'), $this->config->get('config_name'), $this->url->link('information/contact'));
+		}
 
-		$this->data['button_continue'] = $this->language->get('button_continue'); 
+		$this->data['button_continue'] = $this->language->get('button_continue');
 
-		if ($this->cart->hasProducts()) { 
-			$this->data['continue'] = $this->url->link('checkout/cart', '', 'SSL'); 
-		} else { 
-			$this->data['continue'] = $this->url->link('account/account', '', 'SSL'); 
-		} 
+		if ($this->cart->hasProducts()) {
+			$this->data['continue'] = $this->url->link('checkout/cart', '', 'SSL');
+		} else {
+			$this->data['continue'] = $this->url->link('account/account', '', 'SSL');
+		}
 
-		// Custom Template Connector
-		$this->data['template'] = $this->config->get('config_template'); 
+		// Template
+		$this->data['template'] = $this->config->get('config_template');
 
-		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/success.tpl')) { 
-			$this->template = $this->config->get('config_template') . '/template/common/success.tpl'; 
-		} else { 
-			$this->template = 'default/template/common/success.tpl'; 
-		} 
+		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/common/success.tpl')) {
+			$this->template = $this->config->get('config_template') . '/template/common/success.tpl';
+		} else {
+			$this->template = 'default/template/common/success.tpl';
+		}
 
 		$this->children = array(
 			'common/column_left',
@@ -69,7 +64,7 @@ class ControllerAccountSuccess extends Controller {
 			'common/header'
 		);
 
-		$this->response->setOutput($this->render()); 
-	} 
-} 
+		$this->response->setOutput($this->render());
+	}
+}
 ?>
--- upload/catalog/view/theme/default/template/common/success.tpl
+++ upload/catalog/view/theme/default/template/common/success.tpl
@@ -1,16 +1,22 @@
 <?php echo $header; ?>
 <?php echo $column_left; ?><?php echo $column_right; ?>
 <div id="content"><?php echo $content_top; ?>
-	<div class="breadcrumb">
-	<?php foreach ($breadcrumbs as $breadcrumb) { ?>
-		<?php echo $breadcrumb['separator']; ?><a href="<?php echo $breadcrumb['href']; ?>"><?php echo $breadcrumb['text']; ?></a>
-	<?php } ?>
-	</div>
-	<h1><?php echo $heading_title; ?></h1>
-	<?php echo $text_message; ?>
-	<div class="buttons">
-		<div class="right"><a href="<?php echo $continue; ?>" class="button"><?php echo $button_continue; ?></a></div>
-	</div>
-	<?php echo $content_bottom; ?>
+  <div class="breadcrumb">
+  <?php foreach ($breadcrumbs as $breadcrumb) { ?>
+    <?php echo $breadcrumb['separator']; ?><a href="<?php echo $breadcrumb['href']; ?>"><?php echo $breadcrumb['text']; ?></a>
+  <?php } ?>
+  </div>
+  <h1><?php echo $heading_title; ?></h1>
+  <?php echo $text_message; ?>
+  <div class="image-links">
+    <a href="<?php echo $this->url->link('common/home'); ?>"><img src="catalog/view/theme/<?php echo $template; ?>/image/64x64/home.png" alt="" /></a>
+	<a href="<?php echo $this->url->link('account/account', '', 'SSL'); ?>"><img src="catalog/view/theme/<?php echo $template; ?>/image/64x64/document.png" alt="" /></a>
+	<a href="<?php echo $this->url->link('account/download', '', 'SSL'); ?>"><img src="catalog/view/theme/<?php echo $template; ?>/image/64x64/download.png" alt="" /></a>
+	<a href="<?php echo $this->url->link('checkout/cart'); ?>"><img src="catalog/view/theme/<?php echo $template; ?>/image/64x64/cart.png" alt="" /></a>
+  </div>
+  <div class="buttons">
+    <div class="right"><a href="<?php echo $continue; ?>" class="button"><?php echo $button_continue; ?></a></div>
+  </div>
+  <?php echo $content_bottom; ?>
 </div>
 <?php echo $footer; ?>
