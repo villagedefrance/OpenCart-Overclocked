54221beece7254457b35d8e259008232ef7df2a1
Philippe Marion <contact@villagedefrance.net>
2014-07-22 12:08:32 +0100

    More Admin Shipping updates (Part 2)
    More Admin Shipping updates (Part 2)

--- upload/admin/controller/shipping/weight.php
+++ upload/admin/controller/shipping/weight.php
@@ -1,139 +1,134 @@
-<?php 
-//------------------------
-// Overclocked Edition		
-//------------------------
+<?php
+class ControllerShippingWeight extends Controller {
+	private $error = array();
 
-class ControllerShippingWeight extends Controller { 
-	private $error = array(); 
+	public function index() {
+		$this->language->load('shipping/weight');
 
-	public function index() { 
+		$this->document->setTitle($this->language->get('heading_title'));
 
-		$this->language->load('shipping/weight'); 
+		$this->load->model('setting/setting');
 
-		$this->document->setTitle($this->language->get('heading_title')); 
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			$this->model_setting_setting->editSetting('weight', $this->request->post);
 
-		$this->load->model('setting/setting'); 
+			$this->session->data['success'] = $this->language->get('text_success');
 
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) { 
-			$this->model_setting_setting->editSetting('weight', $this->request->post); 
+			if (isset($this->request->post['apply'])) {
+				$this->redirect($this->url->link('shipping/weight', 'token=' . $this->session->data['token'], 'SSL'));
+			} else {
+				$this->redirect($this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL'));
+			}
+		}
 
-			$this->session->data['success'] = $this->language->get('text_success'); 
+		$this->data['heading_title'] = $this->language->get('heading_title');
 
-			if (isset($this->request->post['apply'])) { 
-				$this->redirect($this->url->link('shipping/weight', 'token=' . $this->session->data['token'], 'SSL')); 
-			} else { 
-				$this->redirect($this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL')); 
-			} 
-		} 
+		$this->data['text_none'] = $this->language->get('text_none');
+		$this->data['text_enabled'] = $this->language->get('text_enabled');
+		$this->data['text_disabled'] = $this->language->get('text_disabled');
 
-		$this->data['heading_title'] = $this->language->get('heading_title'); 
+		$this->data['entry_rate'] = $this->language->get('entry_rate');
+		$this->data['entry_tax_class'] = $this->language->get('entry_tax_class');
+		$this->data['entry_status'] = $this->language->get('entry_status');
+		$this->data['entry_sort_order'] = $this->language->get('entry_sort_order');
 
-		$this->data['text_none'] = $this->language->get('text_none'); 
-		$this->data['text_enabled'] = $this->language->get('text_enabled'); 
-		$this->data['text_disabled'] = $this->language->get('text_disabled'); 
+		$this->data['button_save'] = $this->language->get('button_save');
+		$this->data['button_apply'] = $this->language->get('button_apply');
+		$this->data['button_cancel'] = $this->language->get('button_cancel');
 
-		$this->data['entry_rate'] = $this->language->get('entry_rate'); 
-		$this->data['entry_tax_class'] = $this->language->get('entry_tax_class'); 
-		$this->data['entry_status'] = $this->language->get('entry_status'); 
-		$this->data['entry_sort_order'] = $this->language->get('entry_sort_order'); 
+		$this->data['tab_general'] = $this->language->get('tab_general');
 
-		$this->data['button_save'] = $this->language->get('button_save'); 
-		$this->data['button_apply'] = $this->language->get('button_apply'); 
-		$this->data['button_cancel'] = $this->language->get('button_cancel'); 
+		if (isset($this->error['warning'])) {
+			$this->data['error_warning'] = $this->error['warning'];
+		} else {
+			$this->data['error_warning'] = '';
+		}
 
-		$this->data['tab_general'] = $this->language->get('tab_general'); 
-
-		if (isset($this->error['warning'])) { 
-			$this->data['error_warning'] = $this->error['warning']; 
-		} else { 
-			$this->data['error_warning'] = ''; 
-		} 
-
-		$this->data['breadcrumbs'] = array(); 
+		$this->data['breadcrumbs'] = array();
 
 		$this->data['breadcrumbs'][] = array(
-			'text'      => $this->language->get('text_home'),
-			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+			'text'		=> $this->language->get('text_home'),
+			'href'		=> $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
 			'separator' => false
-		); 
+		);
 
 		$this->data['breadcrumbs'][] = array(
-			'text'      => $this->language->get('text_shipping'),
-			'href'      => $this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL'),
+			'text'		=> $this->language->get('text_shipping'),
+			'href'		=> $this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL'),
 			'separator' => ' :: '
-		); 
+		);
 
 		$this->data['breadcrumbs'][] = array(
-			'text'      => $this->language->get('heading_title'),
-			'href'      => $this->url->link('shipping/weight', 'token=' . $this->session->data['token'], 'SSL'),
+			'text'		=> $this->language->get('heading_title'),
+			'href'		=> $this->url->link('shipping/weight', 'token=' . $this->session->data['token'], 'SSL'),
 			'separator' => ' :: '
-		); 
+		);
 
-		$this->data['action'] = $this->url->link('shipping/weight', 'token=' . $this->session->data['token'], 'SSL'); 
+		$this->data['action'] = $this->url->link('shipping/weight', 'token=' . $this->session->data['token'], 'SSL');
 
-		$this->data['cancel'] = $this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL'); 
+		$this->data['cancel'] = $this->url->link('extension/shipping', 'token=' . $this->session->data['token'], 'SSL');
 
-		$this->load->model('localisation/geo_zone'); 
+		$this->load->model('localisation/geo_zone');
 
-		$geo_zones = $this->model_localisation_geo_zone->getGeoZones(); 
+		$geo_zones = $this->model_localisation_geo_zone->getGeoZones();
 
-		foreach ($geo_zones as $geo_zone) { 
-			if (isset($this->request->post['weight_' . $geo_zone['geo_zone_id'] . '_rate'])) { 
-				$this->data['weight_' . $geo_zone['geo_zone_id'] . '_rate'] = $this->request->post['weight_' . $geo_zone['geo_zone_id'] . '_rate']; 
-			} else { 
-				$this->data['weight_' . $geo_zone['geo_zone_id'] . '_rate'] = $this->config->get('weight_' . $geo_zone['geo_zone_id'] . '_rate'); 
-			} 
+		foreach ($geo_zones as $geo_zone) {
+			if (isset($this->request->post['weight_' . $geo_zone['geo_zone_id'] . '_rate'])) {
+				$this->data['weight_' . $geo_zone['geo_zone_id'] . '_rate'] = $this->request->post['weight_' . $geo_zone['geo_zone_id'] . '_rate'];
+			} else {
+				$this->data['weight_' . $geo_zone['geo_zone_id'] . '_rate'] = $this->config->get('weight_' . $geo_zone['geo_zone_id'] . '_rate');
+			}
 
-			if (isset($this->request->post['weight_' . $geo_zone['geo_zone_id'] . '_status'])) { 
-				$this->data['weight_' . $geo_zone['geo_zone_id'] . '_status'] = $this->request->post['weight_' . $geo_zone['geo_zone_id'] . '_status']; 
-			} else { 
-				$this->data['weight_' . $geo_zone['geo_zone_id'] . '_status'] = $this->config->get('weight_' . $geo_zone['geo_zone_id'] . '_status'); 
-			} 
-		} 
+			if (isset($this->request->post['weight_' . $geo_zone['geo_zone_id'] . '_status'])) {
+				$this->data['weight_' . $geo_zone['geo_zone_id'] . '_status'] = $this->request->post['weight_' . $geo_zone['geo_zone_id'] . '_status'];
+			} else {
+				$this->data['weight_' . $geo_zone['geo_zone_id'] . '_status'] = $this->config->get('weight_' . $geo_zone['geo_zone_id'] . '_status');
+			}
+		}
 
-		$this->data['geo_zones'] = $geo_zones; 
+		$this->data['geo_zones'] = $geo_zones;
 
-		if (isset($this->request->post['weight_tax_class_id'])) { 
-			$this->data['weight_tax_class_id'] = $this->request->post['weight_tax_class_id']; 
-		} else { 
-			$this->data['weight_tax_class_id'] = $this->config->get('weight_tax_class_id'); 
-		} 
+		if (isset($this->request->post['weight_tax_class_id'])) {
+			$this->data['weight_tax_class_id'] = $this->request->post['weight_tax_class_id'];
+		} else {
+			$this->data['weight_tax_class_id'] = $this->config->get('weight_tax_class_id');
+		}
 
-		$this->load->model('localisation/tax_class'); 
+		$this->load->model('localisation/tax_class');
 
-		$this->data['tax_classes'] = $this->model_localisation_tax_class->getTaxClasses(); 
+		$this->data['tax_classes'] = $this->model_localisation_tax_class->getTaxClasses();
 
-		if (isset($this->request->post['weight_status'])) { 
-			$this->data['weight_status'] = $this->request->post['weight_status']; 
-		} else { 
-			$this->data['weight_status'] = $this->config->get('weight_status'); 
-		} 
+		if (isset($this->request->post['weight_status'])) {
+			$this->data['weight_status'] = $this->request->post['weight_status'];
+		} else {
+			$this->data['weight_status'] = $this->config->get('weight_status');
+		}
 
-		if (isset($this->request->post['weight_sort_order'])) { 
-			$this->data['weight_sort_order'] = $this->request->post['weight_sort_order']; 
-		} else { 
-			$this->data['weight_sort_order'] = $this->config->get('weight_sort_order'); 
-		} 
+		if (isset($this->request->post['weight_sort_order'])) {
+			$this->data['weight_sort_order'] = $this->request->post['weight_sort_order'];
+		} else {
+			$this->data['weight_sort_order'] = $this->config->get('weight_sort_order');
+		}
 
-		$this->template = 'shipping/weight.tpl'; 
+		$this->template = 'shipping/weight.tpl';
 		$this->children = array(
 			'common/header',
 			'common/footer'
-		); 
+		);
 
-		$this->response->setOutput($this->render()); 
-	} 
+		$this->response->setOutput($this->render());
+	}
 
-	protected function validate() { 
-		if (!$this->user->hasPermission('modify', 'shipping/weight')) { 
-			$this->error['warning'] = $this->language->get('error_permission'); 
-		} 
+	protected function validate() {
+		if (!$this->user->hasPermission('modify', 'shipping/weight')) {
+			$this->error['warning'] = $this->language->get('error_permission');
+		}
 
-		if (!$this->error) { 
-			return true; 
-		} else { 
-			return false; 
-		} 
-	} 
-} 
+		if (!$this->error) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
 ?>
