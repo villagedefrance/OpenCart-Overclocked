05d56abdf54d0b7c18aff1a969c2d0269bca61ae
Philippe Marion <contact@villagedefrance.net>
2014-07-27 15:33:11 +0100

    Admin Order Totals update
    Admin Order Totals update

--- upload/admin/controller/total/shipping.php
+++ upload/admin/controller/total/shipping.php
@@ -1,111 +1,106 @@
-<?php 
-//------------------------
-// Overclocked Edition		
-//------------------------
+<?php
+class ControllerTotalShipping extends Controller {
+	private $error = array();
 
-class ControllerTotalShipping extends Controller { 
-	private $error = array(); 
+	public function index() {
+		$this->language->load('total/shipping');
 
-	public function index() { 
+		$this->document->setTitle($this->language->get('heading_title'));
 
-		$this->language->load('total/shipping'); 
+		$this->load->model('setting/setting');
 
-		$this->document->setTitle($this->language->get('heading_title')); 
+		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
+			$this->model_setting_setting->editSetting('shipping', $this->request->post);
 
-		$this->load->model('setting/setting'); 
+			$this->session->data['success'] = $this->language->get('text_success');
 
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) { 
-			$this->model_setting_setting->editSetting('shipping', $this->request->post); 
+			if (isset($this->request->post['apply'])) {
+				$this->redirect($this->url->link('total/shipping', 'token=' . $this->session->data['token'], 'SSL'));
+			} else {
+				$this->redirect($this->url->link('extension/total', 'token=' . $this->session->data['token'], 'SSL'));
+			}
+		}
 
-			$this->session->data['success'] = $this->language->get('text_success'); 
+		$this->data['heading_title'] = $this->language->get('heading_title');
 
-			if (isset($this->request->post['apply'])) { 
-				$this->redirect($this->url->link('total/shipping', 'token=' . $this->session->data['token'], 'SSL')); 
-			} else { 
-				$this->redirect($this->url->link('extension/total', 'token=' . $this->session->data['token'], 'SSL')); 
-			} 
-		} 
+		$this->data['text_enabled'] = $this->language->get('text_enabled');
+		$this->data['text_disabled'] = $this->language->get('text_disabled');
 
-		$this->data['heading_title'] = $this->language->get('heading_title'); 
+		$this->data['entry_estimator'] = $this->language->get('entry_estimator');
+		$this->data['entry_status'] = $this->language->get('entry_status');
+		$this->data['entry_sort_order'] = $this->language->get('entry_sort_order');
 
-		$this->data['text_enabled'] = $this->language->get('text_enabled'); 
-		$this->data['text_disabled'] = $this->language->get('text_disabled'); 
+		$this->data['button_save'] = $this->language->get('button_save');
+		$this->data['button_apply'] = $this->language->get('button_apply');
+		$this->data['button_cancel'] = $this->language->get('button_cancel');
 
-		$this->data['entry_estimator'] = $this->language->get('entry_estimator'); 
-		$this->data['entry_status'] = $this->language->get('entry_status'); 
-		$this->data['entry_sort_order'] = $this->language->get('entry_sort_order'); 
+		if (isset($this->error['warning'])) {
+			$this->data['error_warning'] = $this->error['warning'];
+		} else {
+			$this->data['error_warning'] = '';
+		}
 
-		$this->data['button_save'] = $this->language->get('button_save'); 
-		$this->data['button_apply'] = $this->language->get('button_apply'); 
-		$this->data['button_cancel'] = $this->language->get('button_cancel'); 
-
-		if (isset($this->error['warning'])) { 
-			$this->data['error_warning'] = $this->error['warning']; 
-		} else { 
-			$this->data['error_warning'] = ''; 
-		} 
-
-		$this->data['breadcrumbs'] = array(); 
+		$this->data['breadcrumbs'] = array();
 
 		$this->data['breadcrumbs'][] = array(
-			'text'      => $this->language->get('text_home'),
-			'href'      => $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
+			'text'		=> $this->language->get('text_home'),
+			'href'		=> $this->url->link('common/home', 'token=' . $this->session->data['token'], 'SSL'),
 			'separator' => false
-		); 
+		);
 
 		$this->data['breadcrumbs'][] = array(
-			'text'      => $this->language->get('text_total'),
-			'href'      => $this->url->link('extension/total', 'token=' . $this->session->data['token'], 'SSL'),
+			'text'		=> $this->language->get('text_total'),
+			'href'		=> $this->url->link('extension/total', 'token=' . $this->session->data['token'], 'SSL'),
 			'separator' => ' :: '
-		); 
+		);
 
 		$this->data['breadcrumbs'][] = array(
-			'text'      => $this->language->get('heading_title'),
-			'href'      => $this->url->link('total/shipping', 'token=' . $this->session->data['token'], 'SSL'),
+			'text'		=> $this->language->get('heading_title'),
+			'href'		=> $this->url->link('total/shipping', 'token=' . $this->session->data['token'], 'SSL'),
 			'separator' => ' :: '
-		); 
+		);
 
-		$this->data['action'] = $this->url->link('total/shipping', 'token=' . $this->session->data['token'], 'SSL'); 
+		$this->data['action'] = $this->url->link('total/shipping', 'token=' . $this->session->data['token'], 'SSL');
 
-		$this->data['cancel'] = $this->url->link('extension/total', 'token=' . $this->session->data['token'], 'SSL'); 
+		$this->data['cancel'] = $this->url->link('extension/total', 'token=' . $this->session->data['token'], 'SSL');
 
-		if (isset($this->request->post['shipping_estimator'])) { 
-			$this->data['shipping_estimator'] = $this->request->post['shipping_estimator']; 
-		} else { 
-			$this->data['shipping_estimator'] = $this->config->get('shipping_estimator'); 
-		} 
+		if (isset($this->request->post['shipping_estimator'])) {
+			$this->data['shipping_estimator'] = $this->request->post['shipping_estimator'];
+		} else {
+			$this->data['shipping_estimator'] = $this->config->get('shipping_estimator');
+		}
 
-		if (isset($this->request->post['shipping_status'])) { 
-			$this->data['shipping_status'] = $this->request->post['shipping_status']; 
-		} else { 
-			$this->data['shipping_status'] = $this->config->get('shipping_status'); 
-		} 
+		if (isset($this->request->post['shipping_status'])) {
+			$this->data['shipping_status'] = $this->request->post['shipping_status'];
+		} else {
+			$this->data['shipping_status'] = $this->config->get('shipping_status');
+		}
 
-		if (isset($this->request->post['shipping_sort_order'])) { 
-			$this->data['shipping_sort_order'] = $this->request->post['shipping_sort_order']; 
-		} else { 
-			$this->data['shipping_sort_order'] = $this->config->get('shipping_sort_order'); 
-		} 
+		if (isset($this->request->post['shipping_sort_order'])) {
+			$this->data['shipping_sort_order'] = $this->request->post['shipping_sort_order'];
+		} else {
+			$this->data['shipping_sort_order'] = $this->config->get('shipping_sort_order');
+		}
 
-		$this->template = 'total/shipping.tpl'; 
+		$this->template = 'total/shipping.tpl';
 		$this->children = array(
 			'common/header',
 			'common/footer'
-		); 
+		);
 
-		$this->response->setOutput($this->render()); 
-	} 
+		$this->response->setOutput($this->render());
+	}
 
-	protected function validate() { 
-		if (!$this->user->hasPermission('modify', 'total/shipping')) { 
-			$this->error['warning'] = $this->language->get('error_permission'); 
-		} 
+	protected function validate() {
+		if (!$this->user->hasPermission('modify', 'total/shipping')) {
+			$this->error['warning'] = $this->language->get('error_permission');
+		}
 
-		if (!$this->error) { 
-			return true; 
-		} else { 
-			return false; 
-		} 
-	} 
-} 
+		if (!$this->error) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
 ?>
