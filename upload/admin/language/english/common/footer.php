f90b0b38117d6cf8f93dfca8d00d60cd3959bd1a
Philippe Marion <contact@villagedefrance.net>
2014-11-17 08:17:23 +0000

    Footer copyright Link update
    Footer copyright Link update

--- upload/admin/language/english/common/footer.php
+++ upload/admin/language/english/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer'] = '<a href="http://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.<br />Version %s';
+$_['text_footer'] = '<a href="http://www.villagedefrance.net">OpenCart OverClocked</a> &copy; 2013-' . date('Y') . ' All Rights Reserved.<br />Version %s';
 ?>
