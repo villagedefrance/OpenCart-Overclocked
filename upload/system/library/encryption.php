94e908c69aa44e935e57e6ab168dcf280b69c758
Philippe Marion <contact@villagedefrance.net>
2015-04-23 01:13:59 +0100

    Encryption Class now using AES
    MCRYPT_RIJNDAEL_128 (AES) + MCRYPT_MODE_CBC

--- upload/system/library/encryption.php
+++ upload/system/library/encryption.php
@@ -1,19 +1,36 @@
 <?php
 final class Encryption {
 	private $key;
-	private $iv;
 
-	public function __construct($key) {
-		$this->key = hash('sha256', $key, true);
-		$this->iv = mcrypt_create_iv(32, MCRYPT_RAND);
-	}
+    public function __construct($key) {
+        $this->key = hash('sha256', $key, true);
+    }
 
-	public function encrypt($value) {
-		return strtr(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->key, $value, MCRYPT_MODE_ECB, $this->iv)), '+/=', '-_,');
-	}
+    public function encrypt($value) {
+        $ivsize = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
 
-	public function decrypt($value) {
-		return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->key, base64_decode(strtr($value, '-_,', '+/=')), MCRYPT_MODE_ECB, $this->iv));
-	}
+        $iv = mcrypt_create_iv($ivsize, MCRYPT_DEV_RANDOM);
+
+        $ciphervalue = mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $this->key, $value, MCRYPT_MODE_CBC, $iv);
+
+        return base64_encode($iv.$ciphervalue);
+    }
+
+    public function decrypt($value) {
+        $value = base64_decode($value);
+
+        $ivsize = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+
+        if (strlen($value) < $ivsize) {
+            throw new Exception('Missing initialization vector!');
+        }
+
+        $iv = substr($value, 0, $ivsize);
+        $value = substr($value, $ivsize);
+
+        $plainvalue = mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $this->key, $value, MCRYPT_MODE_CBC, $iv);
+
+        return rtrim($plainvalue, "\0");
+    }
 }
 ?>
