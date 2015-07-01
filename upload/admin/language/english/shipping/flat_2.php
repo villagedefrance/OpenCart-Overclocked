5d0350ea28c5a434ae313ce237c9b32778ad8d07
Philippe Marion <contact@villagedefrance.net>
2015-07-01 13:16:35 +0100

    Added 3 extra Flat rate shipping
    Added 3 extra Flat rate shipping

---
+++ upload/admin/language/english/shipping/flat_2.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']    		= 'Flat Rate 2';
+
+// Text
+$_['text_shipping']    		= 'Shipping';
+$_['text_success']     		= 'Success: You have modified <b>Flat Rate 2</b> shipping !';
+
+// Entry
+$_['entry_cost']       		= 'Cost:';
+$_['entry_tax_class']  		= 'Tax Class:';
+$_['entry_geo_zone']   		= 'Geo Zone:';
+$_['entry_status']     		= 'Status:';
+$_['entry_sort_order'] 		= 'Sort Order:';
+
+// Error
+$_['error_permission'] 		= 'Warning: You do not have permission to modify <b>Flat Rate 2</b> shipping !';
+?>
---
+++ upload/catalog/model/shipping/flat_2.php
@@ -0,0 +1,42 @@
+<?php
+class ModelShippingFlat2 extends Model {
+
+	function getQuote($address) {
+		$this->language->load('shipping/flat');
+
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('flat_2_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+
+		if (!$this->config->get('flat_2_geo_zone_id')) {
+			$status = true;
+		} elseif ($query->num_rows) {
+			$status = true;
+		} else {
+			$status = false;
+		}
+
+		$method_data = array();
+
+		if ($status) {
+			$quote_data = array();
+
+			$quote_data['flat_2'] = array(
+				'code'         	=> 'flat_2.flat_2',
+				'title'        		=> $this->language->get('text_description'),
+				'cost'         	=> $this->config->get('flat_2_cost'),
+				'tax_class_id' 	=> $this->config->get('flat_2_tax_class_id'),
+				'text'         	=> $this->currency->format($this->tax->calculate($this->config->get('flat_2_cost'), $this->config->get('flat_2_tax_class_id'), $this->config->get('config_tax')))
+			);
+
+			$method_data = array(
+				'code'		=> 'flat_2',
+				'title'			=> $this->language->get('text_title'),
+				'quote'		=> $quote_data,
+				'sort_order'	=> $this->config->get('flat_2_sort_order'),
+				'error'			=> false
+			);
+		}
+
+		return $method_data;
+	}
+}
+?>
