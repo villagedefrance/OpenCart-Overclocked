614adf07f102b3c3d3e893e2dc86d178529f7602
Philippe Marion <contact@villagedefrance.net>
2015-11-15 20:31:13 +0000

    Very minor update ...
    Very minor update ...

--- upload/admin/language/english/report/product_quantity.php
+++ upload/admin/language/english/report/product_quantity.php
@@ -10,8 +10,8 @@ $_['text_view']  			= 'View';
 
 // Column
 $_['column_id']			= 'ID';
-$_['column_name']    	= 'Product Name';
-$_['column_model']   	= 'Model';
+$_['column_name']		= 'Product Name';
+$_['column_model']		= 'Model';
 $_['column_price']    	= 'Price';
 $_['column_cost']   		= 'Cost';
 $_['column_status']   	= 'Status';
