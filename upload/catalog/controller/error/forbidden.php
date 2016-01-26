f9c2c50cd762a312a0cac039a12c10d470e56ab4
Philippe Marion <contact@villagedefrance.net>
2016-01-26 19:47:32 +0000

    Force Logout for blocked ips
    Force Logout for blocked ips

--- upload/catalog/controller/error/forbidden.php
+++ upload/catalog/controller/error/forbidden.php
@@ -12,6 +12,30 @@ class ControllerErrorForbidden extends Controller {
 			}
 		}
 
+		if ($this->customer->isLogged()) {
+			$this->customer->logout();
+			$this->cart->clear();
+
+			unset($this->session->data['wishlist']);
+			unset($this->session->data['shipping_address_id']);
+			unset($this->session->data['shipping_country_id']);
+			unset($this->session->data['shipping_zone_id']);
+			unset($this->session->data['shipping_postcode']);
+			unset($this->session->data['shipping_method']);
+			unset($this->session->data['shipping_methods']);
+			unset($this->session->data['payment_address_id']);
+			unset($this->session->data['payment_country_id']);
+			unset($this->session->data['payment_zone_id']);
+			unset($this->session->data['payment_method']);
+			unset($this->session->data['payment_methods']);
+			unset($this->session->data['comment']);
+			unset($this->session->data['order_id']);
+			unset($this->session->data['coupon']);
+			unset($this->session->data['reward']);
+			unset($this->session->data['voucher']);
+			unset($this->session->data['vouchers']);
+		}
+
 		// Show site if logged in as admin
 		$this->load->library('user');
 
