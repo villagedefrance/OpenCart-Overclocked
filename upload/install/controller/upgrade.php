07e7963691b6efee41716e81cf89b9552c985c86
Philippe Marion <contact@villagedefrance.net>
2016-03-06 13:42:08 +0000

    Upgrade controller update
    Upgrade controller update

--- upload/install/controller/upgrade.php
+++ upload/install/controller/upgrade.php
@@ -62,7 +62,7 @@ class ControllerUpgrade extends Controller {
 		$status = false;
 
 		// Check if the sql file exists
-		$file = DIR_APPLICATION . 'opencart-clean.sql';
+		$file = DIR_APPLICATION . 'opencart-upgrade.sql';
 
 		if (!file_exists($file)) {
 			exit('Could not load sql file: ' . $file);
