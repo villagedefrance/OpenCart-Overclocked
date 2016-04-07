38336d49ebb3898d24dcdd080d23eeef76168cfa
Philippe Marion <contact@villagedefrance.net>
2016-04-07 18:19:38 +0100

    Version numbers rolled
    Version numbers rolled

--- upload/admin/index.php
+++ upload/admin/index.php
@@ -1,8 +1,8 @@
 <?php
 // Version
-define('VERSION', '1.7.6 RC1');
-define('REVISION', 20160329);
-define('RELEASED', '2016-03-29');
+define('VERSION', '1.7.6');
+define('REVISION', 20160407);
+define('RELEASED', '2016-04-07');
 
 // Configuration
 if (file_exists('config.php')) {
--- upload/index.php
+++ upload/index.php
@@ -1,6 +1,6 @@
 <?php
 // Version
-define('VERSION', '1.7.6 RC1');
+define('VERSION', '1.7.6');
 
 // Configuration
 if (file_exists('config.php')) {
