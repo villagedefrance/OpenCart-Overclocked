9dee60c33f85c09dd2d44b37c649073ee5a42234
Philippe Marion <contact@villagedefrance.net>
2016-05-27 17:08:03 +0100

    Code alignment test ...
    Code alignment test ...

--- upload/admin/language/english/sale/supplier_group.php
+++ upload/admin/language/english/sale/supplier_group.php
@@ -1,43 +1,43 @@
 <?php
 // Heading
-$_['heading_title']         			= 'Supplier Group';
+$_['heading_title']             = 'Supplier Group';
 
 // Text
-$_['text_success']         			= 'Success: You have modified <b>Supplier Groups</b> !';
+$_['text_success']              = 'Success: You have modified <b>Supplier Groups</b> !';
 
 // Text Order
-$_['text_order_email'] 				= 'Email';
-$_['text_order_phone'] 				= 'Telephone';
-$_['text_order_fax'] 					= 'Fax';
-$_['text_order_post'] 				= 'Post';
-$_['text_order_instore'] 			= 'In Store';
-$_['text_order_other'] 				= 'Other';
+$_['text_order_email']          = 'Email';
+$_['text_order_phone']          = 'Telephone';
+$_['text_order_fax']            = 'Fax';
+$_['text_order_post']           = 'Post';
+$_['text_order_instore']        = 'In Store';
+$_['text_order_other']          = 'Other';
 
 // Text Payment
-$_['text_payment_account'] 		= 'Account';
-$_['text_payment_cash'] 			= 'Cash';
-$_['text_payment_cheque'] 		= 'Cheque';
-$_['text_payment_creditcard'] 	= 'Credit Card';
-$_['text_payment_banktransfer']	= 'Bank Transfer';
-$_['text_payment_other'] 			= 'Other';
+$_['text_payment_account']      = 'Account';
+$_['text_payment_cash']         = 'Cash';
+$_['text_payment_cheque']       = 'Cheque';
+$_['text_payment_creditcard']   = 'Credit Card';
+$_['text_payment_banktransfer'] = 'Bank Transfer';
+$_['text_payment_other']        = 'Other';
 
 // Column
-$_['column_name']          			= 'Supplier Group Name';
-$_['column_order_method'] 		= 'Order Method';
-$_['column_payment_method'] 	= 'Payment Method';
-$_['column_sort_order']    			= 'Sort Order';
-$_['column_action']        			= 'Action';
+$_['column_name']               = 'Supplier Group Name';
+$_['column_order_method']       = 'Order Method';
+$_['column_payment_method']     = 'Payment Method';
+$_['column_sort_order']         = 'Sort Order';
+$_['column_action']             = 'Action';
 
 // Entry
-$_['entry_name']             			= 'Supplier Group Name:';
-$_['entry_description']     			= 'Description:';
-$_['entry_order_method'] 			= 'Order Method:';
-$_['entry_payment_method'] 		= 'Payment Method:';
-$_['entry_sort_order']          		= 'Sort Order:';
+$_['entry_name']                = 'Supplier Group Name:';
+$_['entry_description']         = 'Description:';
+$_['entry_order_method']        = 'Order Method:';
+$_['entry_payment_method']      = 'Payment Method:';
+$_['entry_sort_order']          = 'Sort Order:';
 
 // Error
-$_['error_permission']     			= 'Warning: You do not have permission to modify <b>Supplier Groups</b> !';
-$_['error_name']           			= 'Supplier Group Name must be between 3 and 32 characters!';
-$_['error_default']             		= 'Warning: This supplier group cannot be deleted as it is currently assigned as the default supplier group!';
-$_['error_supplier']       			= 'Warning: This supplier group cannot be deleted as it is currently assigned to %s suppliers!';
+$_['error_permission']          = 'Warning: You do not have permission to modify <b>Supplier Groups</b> !';
+$_['error_name']                = 'Supplier Group Name must be between 3 and 32 characters!';
+$_['error_default']             = 'Warning: This supplier group cannot be deleted as it is currently assigned as the default supplier group!';
+$_['error_supplier']            = 'Warning: This supplier group cannot be deleted as it is currently assigned to %s suppliers!';
 ?>
--- upload/admin/language/english/sale/supplier_product.php
+++ upload/admin/language/english/sale/supplier_product.php
@@ -1,73 +1,73 @@
 <?php
 // Heading
-$_['heading_title']          		= 'Supplier Products';
-$_['heading_dimension'] 		= 'Dimensions';
+$_['heading_title']           = 'Supplier Products';
+$_['heading_dimension']       = 'Dimensions';
 
 // Text  
-$_['text_success']           		= 'Success: You have modified <b>Supplier Products</b> !';
-$_['text_default']           		= 'Default';
-$_['text_image_manager'] 		= 'File Manager';
-$_['text_browse']            		= 'Browse';
-$_['text_clear']             		= 'Clear';
-$_['text_autocomplete']     	= '<span class="help">(Autocomplete)</span>';
+$_['text_success']            = 'Success: You have modified <b>Supplier Products</b> !';
+$_['text_default']            = 'Default';
+$_['text_image_manager']      = 'File Manager';
+$_['text_browse']             = 'Browse';
+$_['text_clear']              = 'Clear';
+$_['text_autocomplete']       = '<span class="help">(Autocomplete)</span>';
 
 // Column Form
-$_['column_text'] 				= 'Text';
-$_['column_weight']				= 'Weight';
-$_['column_supplier_group']	= 'Supplier Group';
-$_['column_quantity']        	= 'Quantity';
-$_['column_price']           		= 'Price';
-$_['column_image']           	= 'Image';
+$_['column_text']             = 'Text';
+$_['column_weight']           = 'Weight';
+$_['column_supplier_group']   = 'Supplier Group';
+$_['column_quantity']         = 'Quantity';
+$_['column_price']            = 'Price';
+$_['column_image']            = 'Image';
 
 // Column List
-$_['column_id']					= 'Id';
-$_['column_name']				= 'Product Name';
-$_['column_model']				= 'Model';
-$_['column_supplier']			= 'Supplier';
-$_['column_status']				= 'Status';
-$_['column_action']				= 'Action';
+$_['column_id']               = 'Id';
+$_['column_name']             = 'Product Name';
+$_['column_model']            = 'Model';
+$_['column_supplier']         = 'Supplier';
+$_['column_status']           = 'Status';
+$_['column_action']           = 'Action';
 
 // Entry
-$_['entry_supplier']             	= 'Product Supplier:';
-$_['entry_name']             		= 'Product Name:';
-$_['entry_model']            		= 'Model:';
-$_['entry_manufacturer']     	= 'Manufacturer:';
-$_['entry_image']            		= 'Image:';
-$_['entry_price']            		= 'Price:';
-$_['entry_tax_class']        	= 'Tax Class:';
-$_['entry_unit']            		= 'Units:<span class="help">If the product contains multiples, enter the number of single units. Otherwise, enter "1" (default).</span>';
-$_['entry_color']            		= 'Colour:<span class="help">(Optional)</span>';
-$_['entry_size']            		= 'Size:<span class="help">(Optional)</span>';
-$_['entry_quantity']         		= 'Minimum Order Quantity:<span class="help">(Optional)</span>';
-$_['entry_dimension']        	= 'Dimensions (L x W x H):';
-$_['entry_length']           		= 'Length Class:';
-$_['entry_weight']           		= 'Weight:';
-$_['entry_weight_class']     	= 'Weight Class:';
-$_['entry_status']           		= 'Status:';
+$_['entry_supplier']          = 'Product Supplier:';
+$_['entry_name']              = 'Product Name:';
+$_['entry_model']             = 'Model:';
+$_['entry_manufacturer']      = 'Manufacturer:';
+$_['entry_image']             = 'Image:';
+$_['entry_price']             = 'Price:';
+$_['entry_tax_class']         = 'Tax Class:';
+$_['entry_unit']              = 'Units:<span class="help">If the product contains multiples, enter the number of single units. Otherwise, enter "1" (default).</span>';
+$_['entry_color']             = 'Colour:<span class="help">(Optional)</span>';
+$_['entry_size']              = 'Size:<span class="help">(Optional)</span>';
+$_['entry_quantity']          = 'Minimum Order Quantity:<span class="help">(Optional)</span>';
+$_['entry_dimension']         = 'Dimensions (L x W x H):';
+$_['entry_length']            = 'Length Class:';
+$_['entry_weight']            = 'Weight:';
+$_['entry_weight_class']      = 'Weight Class:';
+$_['entry_status']            = 'Status:';
 
 // Mass Update
-$_['text_selected_yes']			= 'Selected only';
-$_['text_selected_no']			= 'All Products';
-$_['text_price_title']				= 'Update Product Price';
-$_['text_price_success']		= 'Success: you have updated <b>Product Price</b> of selected products';
+$_['text_selected_yes']       = 'Selected only';
+$_['text_selected_no']        = 'All Products';
+$_['text_price_title']        = 'Update Product Price';
+$_['text_price_success']      = 'Success: you have updated <b>Product Price</b> of selected products';
 
-$_['entry_px_selected']			= 'Products to update:';
-$_['entry_px_price']				= 'Enter Purchase Price:';
+$_['entry_px_selected']       = 'Products to update:';
+$_['entry_px_price']          = 'Enter Purchase Price:';
 
-$_['button_refresh']				= 'Refresh';
-$_['button_update_price']		= 'Prices';
+$_['button_refresh']          = 'Refresh';
+$_['button_update_price']     = 'Prices';
 
-$_['error_selected']				= 'No products are currently selected!';
-$_['error_price']					= 'Price is required and cannot be negative!';
+$_['error_selected']          = 'No products are currently selected!';
+$_['error_price']             = 'Price is required and cannot be negative!';
 
 // Button
-$_['button_new_supplier'] 			= 'New supplier ?';
-$_['button_new_manufacturer']	= 'New manufacturer ?';
+$_['button_new_supplier']     = 'New supplier ?';
+$_['button_new_manufacturer'] = 'New manufacturer ?';
 
 // Error
-$_['error_permission']       		= 'Warning: You do not have permission to modify <b>Supplier Products</b> !';
-$_['error_warning']          		= 'Warning: Please check the form carefully for errors!';
-$_['error_supplier_id']			= 'Product Supplier is required!';
-$_['error_name']             		= 'Product Name must be greater than 3 and less than 255 characters!';
-$_['error_model']            		= 'Product Model must be greater than 3 and less than 64 characters!';
+$_['error_permission']        = 'Warning: You do not have permission to modify <b>Supplier Products</b> !';
+$_['error_warning']           = 'Warning: Please check the form carefully for errors!';
+$_['error_supplier_id']       = 'Product Supplier is required!';
+$_['error_name']              = 'Product Name must be greater than 3 and less than 255 characters!';
+$_['error_model']             = 'Product Model must be greater than 3 and less than 64 characters!';
 ?>
