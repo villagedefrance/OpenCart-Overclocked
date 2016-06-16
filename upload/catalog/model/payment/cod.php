2bdec1c9005209b7727b35de86aed184077da1cd
gob33 <pblain@free.fr>
2016-06-16 14:26:34 +0200

    hasShipping added

--- upload/catalog/model/payment/cod.php
+++ upload/catalog/model/payment/cod.php
@@ -1,37 +1,39 @@
 <?php
 class ModelPaymentCOD extends Model {
 
-	public function getMethod($address, $total) {
-		$this->language->load('payment/cod');
+  public function getMethod($address, $total) {
+    $this->language->load('payment/cod');
 
-		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('cod_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
+    $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "zone_to_geo_zone WHERE geo_zone_id = '" . (int)$this->config->get('cod_geo_zone_id') . "' AND country_id = '" . (int)$address['country_id'] . "' AND (zone_id = '" . (int)$address['zone_id'] . "' OR zone_id = '0')");
 
-		$total_max = $this->config->get('cod_total_max');
+    $total_max = $this->config->get('cod_total_max');
 
-		if (($this->config->get('cod_total') > 0 && $this->config->get('cod_total') > $total) || (!empty($total_max) && $total_max > 0 && $total_max < $total)) {
-			$status = false;
-		} elseif ($this->config->has('cod_total_max') && $this->config->get('cod_total_max') > 0 && $total > $this->config->get('cod_total_max')) {
-			$status = false;
-		} elseif (!$this->config->get('cod_geo_zone_id')) {
-			$status = true;
-		} elseif ($query->num_rows) {
-			$status = true;
-		} else {
-			$status = false;
-		}
+    if (($this->config->get('cod_total') > 0 && $this->config->get('cod_total') > $total) || (!empty($total_max) && $total_max > 0 && $total_max < $total)) {
+      $status = false;
+    } elseif ($this->config->has('cod_total_max') && $this->config->get('cod_total_max') > 0 && $total > $this->config->get('cod_total_max')) {
+      $status = false;
+    } elseif (!$this->cart->hasShipping()) {
+      $status = false;
+    } elseif (!$this->config->get('cod_geo_zone_id')) {
+      $status = true;
+    } elseif ($query->num_rows) {
+      $status = true;
+    } else {
+      $status = false;
+    }
 
-		$method_data = array();
+    $method_data = array();
 
-		if ($status) {
-			$method_data = array(
-				'code'       => 'cod',
-				'title'      => $this->language->get('text_title'),
-				'terms'      => '',
-				'sort_order' => $this->config->get('cod_sort_order')
-			);
-		}
+    if ($status) {
+      $method_data = array(
+        'code'       => 'cod',
+        'title'      => $this->language->get('text_title'),
+        'terms'      => '',
+        'sort_order' => $this->config->get('cod_sort_order')
+      );
+    }
 
-		return $method_data;
-	}
+    return $method_data;
+  }
 }
 ?>
