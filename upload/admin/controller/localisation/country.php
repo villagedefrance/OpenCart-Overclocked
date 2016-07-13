09fc579acf23bde3a62a5fe9d950e02d7869984e
gob33 <pblain@free.fr>
2016-07-13 16:26:15 +0200

    Correction

--- upload/admin/controller/localisation/country.php
+++ upload/admin/controller/localisation/country.php
@@ -113,7 +113,7 @@ class ControllerLocalisationCountry extends Controller {
 
 		if (isset($this->request->post['selected']) && $this->validate()) {
 			foreach ($this->request->post['selected'] as $country_id) {
-			  	$country_info = $this->model_localisation_country->getCountry($country_id);
+					$country_info = $this->model_localisation_country->getCountry($country_id);
 
 				if ($country_info && !$country_info['status']) {
 					$this->model_localisation_country->enableCountry($country_id);
@@ -155,7 +155,7 @@ class ControllerLocalisationCountry extends Controller {
 
 		if (isset($this->request->post['selected']) && $this->validate()) {
 			foreach ($this->request->post['selected'] as $country_id) {
-			  	$country_info = $this->model_localisation_country->getCountry($country_id);
+					$country_info = $this->model_localisation_country->getCountry($country_id);
 
 				if ($country_info && $country_info['status']) {
 					$this->model_localisation_country->disableCountry($country_id);
@@ -202,7 +202,7 @@ class ControllerLocalisationCountry extends Controller {
 
 			$this->session->data['success'] = $this->language->get('text_success');
 
-			$url = ''; 
+			$url = '';
 
 			if (isset($this->request->get['filter_name'])) {
 				$url .= '&filter_name=' . urlencode(html_entity_decode($this->request->get['filter_name'], ENT_QUOTES, 'UTF-8'));
@@ -631,6 +631,32 @@ class ControllerLocalisationCountry extends Controller {
 		} else {
 			return false;
 		}
+	}
+
+	public function country() {
+		$json = array();
+
+		$this->load->model('localisation/country');
+
+		$country_info = $this->model_localisation_country->getCountry($this->request->get['country_id']);
+
+		if ($country_info) {
+			$this->load->model('localisation/zone');
+
+			$json = array(
+				'country_id'        => $country_info['country_id'],
+				'name'              => $country_info['name'],
+				'iso_code_2'        => $country_info['iso_code_2'],
+				'iso_code_3'        => $country_info['iso_code_3'],
+				'address_format'    => $country_info['address_format'],
+				'postcode_required' => $country_info['postcode_required'],
+				'zone'              => $this->model_localisation_zone->getZonesByCountryId($this->request->get['country_id']),
+				'status'            => $country_info['status']
+			);
+		}
+
+		$this->response->addHeader('Content-Type: application/json');
+		$this->response->setOutput(json_encode($json));
 	}
 
 	public function autocomplete() {
