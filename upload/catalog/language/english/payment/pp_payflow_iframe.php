ad399b81ff50c10cde451f485e959d8e9cbafe8b
Philippe Marion <contact@villagedefrance.net>
2016-07-22 16:29:00 +0100

    Minor Payment Language updates
    Minor Payment Language updates

--- upload/catalog/language/english/payment/pp_payflow_iframe.php
+++ upload/catalog/language/english/payment/pp_payflow_iframe.php
@@ -3,6 +3,6 @@
 $_['text_title']             = 'Credit or Debit Card';
 $_['text_secure_connection'] = 'Creating a secure connection...';
 
-//Errors
+// Error
 $_['error_connection']       = "Could not connect to PayPal. Please contact the shop's administrator for assistance or choose a different payment method.";
 ?>
