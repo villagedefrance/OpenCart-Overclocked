d671ee8b89d2fd0b1cda60f6f69983b7f2b1e8d9
Philippe Marion <contact@villagedefrance.net>
2016-08-21 09:27:08 +0100

    Extra code validation
    Extra code validation

--- upload/admin/view/javascript/jquery/jstree/plugins/jquery.tree.contextmenu.js
+++ upload/admin/view/javascript/jquery/jstree/plugins/jquery.tree.contextmenu.js
@@ -12,23 +12,23 @@
 				class_name : "hover",
 				items : {
 					create : {
-						label	: "Create",
-						icon	: "create",
-						visible	: function (NODE, TREE_OBJ) { if (NODE.length != 1) return 0; return TREE_OBJ.check("creatable", NODE); },
-						action	: function (NODE, TREE_OBJ) { TREE_OBJ.create(false, TREE_OBJ.get_node(NODE[0])); },
+						label : "Create",
+						icon : "create",
+						visible : function (NODE, TREE_OBJ) { if (NODE.length != 1) return 0; return TREE_OBJ.check("creatable", NODE); },
+						action : function (NODE, TREE_OBJ) { TREE_OBJ.create(false, TREE_OBJ.get_node(NODE[0])); },
 						separator_after : true
 					},
 					rename : {
-						label	: "Rename",
-						icon	: "rename",
-						visible	: function (NODE, TREE_OBJ) { if (NODE.length != 1) return false; return TREE_OBJ.check("renameable", NODE); },
-						action	: function (NODE, TREE_OBJ) { TREE_OBJ.rename(NODE); }
+						label : "Rename",
+						icon : "rename",
+						visible : function (NODE, TREE_OBJ) { if (NODE.length != 1) return false; return TREE_OBJ.check("renameable", NODE); },
+						action : function (NODE, TREE_OBJ) { TREE_OBJ.rename(NODE); }
 					},
 					remove : {
-						label	: "Delete",
-						icon	: "remove",
-						visible	: function (NODE, TREE_OBJ) { var ok = true; $.each(NODE, function (){ if (TREE_OBJ.check("deletable", this) == false) ok = false; return false; }); return ok; },
-						action	: function (NODE, TREE_OBJ) { $.each(NODE, function (){ TREE_OBJ.remove(this); }); }
+						label : "Delete",
+						icon : "remove",
+						visible : function (NODE, TREE_OBJ) { var ok = true; $.each(NODE, function (){ if (TREE_OBJ.check("deletable", this) == false) ok = false; return false; }); return ok; },
+						action : function (NODE, TREE_OBJ) { $.each(NODE, function (){ TREE_OBJ.remove(this); }); }
 					}
 				}
 			},
