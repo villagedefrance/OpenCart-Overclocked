4487d8a7c5cb2abd94de8f2f67458babb2da03d6
Philippe Marion <contact@villagedefrance.net>
2016-08-23 16:11:55 +0100

    Minor language update
    Minor language update

--- upload/admin/language/english/feed/storeya.php
+++ upload/admin/language/english/feed/storeya.php
@@ -5,7 +5,7 @@ $_['heading_title']    = 'StoreYa';
 // Text
 $_['text_feed']        = 'Product Feeds';
 $_['text_success']     = 'Success: You have modified <b>StoreYa</b> feed!';
-$_['text_link']        = '<span class="help">Go to StoreYa page, select a plan and paste the Data Feed Url link !</span>';
+$_['text_link']        = '<span class="help">Click to go to the StoreYa page, choose a plan and then paste the link in the "Data Feed Url" field above!</span>';
 
 // Entry
 $_['entry_status']     = 'Status:';
