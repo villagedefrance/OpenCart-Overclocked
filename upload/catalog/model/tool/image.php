21d67c570e82b04806c9b0ffc65f84253be267e0
Philippe Marion <contact@villagedefrance.net>
2016-10-24 15:02:55 +0100

    Added hex2rgba image function
    Added hex2rgba image function

--- upload/catalog/model/tool/image.php
+++ upload/catalog/model/tool/image.php
@@ -45,4 +45,38 @@ class ModelToolImage extends Model {
 			return $this->config->get('config_url') . 'image/' . $new_image;
 		}
 	}
+
+	public function hex2rgba($color, $opacity) {
+		$default = 'rgb(0,0,0)';
+ 
+		if (empty($color)) {
+			return $default;
+		}
+ 
+		if ($color[0] == '#') {
+			$color = substr($color, 1);
+		}
+ 
+		if (strlen($color) == 6) {
+			$hex = array($color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5]);
+		} elseif (strlen($color ) == 3) {
+			$hex = array($color[0] . $color[0], $color[1] . $color[1], $color[2] . $color[2]);
+		} else {
+			return $default;
+		}
+
+		$rgb = array_map('hexdec', $hex);
+
+		if ($opacity) {
+			if (abs($opacity) > 1) {
+				$opacity = 1;
+			}
+
+			$output = 'rgba(' . implode(",", $rgb) . ',' . $opacity . ')';
+		} else {
+			$output = 'rgb(' . implode(",", $rgb) . ')';
+		}
+
+		return $output;
+	}
 }
