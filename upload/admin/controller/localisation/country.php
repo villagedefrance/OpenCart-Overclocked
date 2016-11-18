9d9b268cb7dad9cd953bc32b4f7abfc0ede57b49
Philippe Marion <contact@villagedefrance.net>
2016-11-18 10:09:48 +0000

    Country + Zones filter name small fix
    Country + Zones filter name small fix

--- upload/admin/controller/localisation/country.php
+++ upload/admin/controller/localisation/country.php
@@ -450,6 +450,10 @@ class ControllerLocalisationCountry extends Controller {
 
 		$url = '';
 
+		if (isset($this->request->get['filter_name'])) {
+			$url .= '&filter_name=' . urlencode(html_entity_decode($this->request->get['filter_name'], ENT_QUOTES, 'UTF-8'));
+		}
+
 		if (isset($this->request->get['sort'])) {
 			$url .= '&sort=' . $this->request->get['sort'];
 		}
