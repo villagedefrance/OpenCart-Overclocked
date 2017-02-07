94b37dea799e37677ef73798e29b62ed5bb88b3d
Philippe Marion <contact@villagedefrance.net>
2017-02-07 15:17:02 +0000

    Lighter Chart Background
    Lighter Chart Background

--- upload/admin/controller/common/home.php
+++ upload/admin/controller/common/home.php
@@ -234,7 +234,7 @@ class ControllerCommonHome extends Controller {
 		$admin_css = $this->config->get('config_admin_stylesheet');
 
 		if ($admin_css == 'overclock') {
-			$this->data['chart_background'] = '#4B515A';
+			$this->data['chart_background'] = '#6D7582';
 			$this->data['chart_border'] = '#999999';
 			$this->data['chart_colour'] = '#E5E5E5';
 		} elseif ($admin_css == 'classic') {
