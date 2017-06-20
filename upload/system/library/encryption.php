6d8935af525c71581cd8e9cc6697deeee97210e7
Philippe Marion <contact@villagedefrance.net>
2017-06-20 10:32:16 +0100

    Encryption library update
    Mcrypt is deprecated after PHP 7.1, so added OpenSSL encryption as well.

--- upload/system/library/encryption.php
+++ upload/system/library/encryption.php
@@ -7,19 +7,30 @@ final class Encryption {
 	}
 
 	public function encrypt($value) {
-		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
-
 		$php_version = phpversion();
 
-		if ($php_version >= '5.6') {
-			$iv = mcrypt_create_iv($iv_size, MCRYPT_DEV_URANDOM);
-		} elseif ($php_version >= '5.3') {
-			$iv = mcrypt_create_iv($iv_size, MCRYPT_DEV_RANDOM);
-		} else {
-			$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
-		}
+		if ($php_version >= '7.1') {
+			$method = 'AES-128-CBC';
 
-		$encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $this->key, true), $value, MCRYPT_MODE_CBC, $iv);
+			$iv_length = openssl_cipher_iv_length($method); // 16
+
+			$iv = openssl_random_pseudo_bytes($iv_length);
+
+			$encrypted = openssl_encrypt($value, $method, hash('sha256', $this->key, true), OPENSSL_RAW_DATA, $iv);
+
+		} else {
+			$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+
+			if ($php_version >= '5.6') {
+				$iv = mcrypt_create_iv($iv_size, MCRYPT_DEV_URANDOM);
+			} elseif ($php_version >= '5.3') {
+				$iv = mcrypt_create_iv($iv_size, MCRYPT_DEV_RANDOM);
+			} else {
+				$iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+			}
+
+			$encrypted = mcrypt_encrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $this->key, true), $value, MCRYPT_MODE_CBC, $iv);
+		}
 
 		$encoded = base64_encode($encrypted) . '|' . base64_encode($iv);
 
@@ -27,19 +38,30 @@ final class Encryption {
 	}
 
 	public function decrypt($value) {
+		$php_version = phpversion();
+
 		$value = explode('|', strtr($value, '-_,', '+/=') . '|');
 
 		$decoded = base64_decode($value[0]);
 
 		$iv = base64_decode($value[1]);
 
-		$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+		if ($php_version >= '7.1') {
+			$method = 'AES-128-CBC';
 
-		if (strlen($iv) !== $iv_size) {
-			return false;
+			$iv_length = openssl_cipher_iv_length($method); // 16
+
+			$decrypted = trim(openssl_decrypt($decoded, $method, hash('sha256', $this->key, true), OPENSSL_RAW_DATA, $iv));
+
+		} else {
+			$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CBC);
+
+			if (strlen($iv) !== $iv_size) {
+				return false;
+			}
+
+			$decrypted = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $this->key, true), $decoded, MCRYPT_MODE_CBC, $iv));
 		}
-
-		$decrypted = trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, hash('sha256', $this->key, true), $decoded, MCRYPT_MODE_CBC, $iv));
 
 		return $decrypted;
 	}
