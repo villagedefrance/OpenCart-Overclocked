1aa41a6222b5f8da36ada19c522a7f0bc3b2fafe
Philippe Marion <contact@villagedefrance.net>
2018-02-18 19:21:04 +0000

    OpenBay Pro message fix #128
    Redirection message now fixed.

--- upload/admin/language/english/modification/openbaypro.php
+++ upload/admin/language/english/modification/openbaypro.php
@@ -4,4 +4,4 @@ $_['heading_title']     = 'OpenBay Pro';
 
 // Text
 $_['text_modification'] = 'Modifications';
-$_['text_installed']    = 'OpenBay Pro is now installed. It is available under <b>Extensions > OpenBay Pro</b>';
+$_['text_installed']    = 'OpenBay Pro is now installed. It is available under <b>Tools > OpenBay Pro</b>';
